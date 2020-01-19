dd8de8ca420cc86bb4449215216781f00b43d131
Felix <felix.lapierre1@gmail.com>
2020-01-19 01:35:06 -0500

    Created a basic unit test for add

---
+++ app/src/test/java/com/example/campusguide/utils/CalculatorTest.kt
@@ -0,0 +1,16 @@
+package com.example.campusguide.utils
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class CalculatorTest {
+
+    @Test
+    fun add() {
+        var first = 5;
+        var second = 10;
+        var expected = 15;
+        assertEquals(expected, Calculator().add(first, second));
+    }
+}
