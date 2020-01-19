b9c919b38310d629f9ead420ad3b3456db6414b6
Maxim Pobudzey <maxim.pobudzey@net4user.com>
2020-01-19 18:28:02 -0500

    Added current location button

--- app/src/main/java/com/example/campusguide/MapsActivity.kt
+++ app/src/main/java/com/example/campusguide/MapsActivity.kt
@@ -2,6 +2,7 @@ package com.example.campusguide
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -21,6 +22,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        val currentLocationButton: ImageButton = findViewById(R.id.currentLocationButton)
     }
 
     /**
