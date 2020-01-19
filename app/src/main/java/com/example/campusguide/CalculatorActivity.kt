ea6a221075d557e056610e2bc8b93f73c5a53f7b
Felix <felix.lapierre1@gmail.com>
2020-01-19 00:32:06 -0500

    Created a Calculator activity for which tests shall be written

---
+++ app/src/main/java/com/example/campusguide/CalculatorActivity.kt
@@ -0,0 +1,22 @@
+package com.example.campusguide
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_calculator.*
+
+class CalculatorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculator)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+}
