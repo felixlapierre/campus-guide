ac94c129f707c8eadfcc94f6e7c9e9b6aeaecb1f
gdzooks <issa4044@gmail.com>
2020-01-26 11:42:29 -0500

    Add switch campus functionality to button (buggy, send help)

--- app/src/main/java/com/example/campusguide/MapsActivity.kt
+++ app/src/main/java/com/example/campusguide/MapsActivity.kt
@@ -2,17 +2,19 @@ package com.example.campusguide
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.gms.maps.*
 
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +23,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
     }
 
     /**
@@ -39,5 +43,38 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val hall = LatLng(45.497290, -73.578824)
         mMap.addMarker(MarkerOptions().position(hall).title("Hall Building"))
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(hall, 17.0f))
+
+        // Update switch campus button text value now that map loads
+        val switchCampusButton = findViewById<Button>(R.id.switchCampusButton)
+        switchCampusButton.text = "loy"
+        val switchCampusFAB: FloatingActionButton = findViewById(R.id.switchCampusButton)
+        switchCampusFAB.setOnClickListener(switchCampus())
+    }
+
+    /**
+     * Switch campus method.
+     * Check what campus to switch to, recenter, and change the value of the campus name string
+     * in values/strings.
+     */
+    fun switchCampus() {
+        // get button so we can see it's current text
+        val switchCampusButton = findViewById<Button>(R.id.switchCampusButton)
+
+        fusedLocationClient.lastLocation.addOnSuccessListener(this) {location ->
+            if(switchCampusButton.text.equals("loy")){
+                switchCampusButton.text = "sgw"
+                val loyCoord = LatLng(45.458153, -73.640490)
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(loyCoord, 17.0f))
+
+            } else if(switchCampusButton.text.equals("sgw")){
+                switchCampusButton.text = "loy"
+                val sgwCoord = LatLng(45.495792, -73.578096)
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sgwCoord, 17.0f))
+            }
+            // don't do anything if values are off
+        }
+
+
+
     }
 }
