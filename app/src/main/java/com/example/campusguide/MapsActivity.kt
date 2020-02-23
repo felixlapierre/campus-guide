85f1dfe8990927af2208fcaca891ed7565352108
w.mike.brichko@gmail.com <w.mike.brichko@gmail.com>
2020-02-23 15:42:08 -0500

    Removed files

--- app/src/main/java/com/example/campusguide/MapsActivity.kt
+++ app/src/main/java/com/example/campusguide/MapsActivity.kt
@@ -70,6 +70,15 @@ class MapsActivity() : FragmentActivity(), OnMapReadyCallback, LocationListener,
         campus_name.text = "SGW Campus"
     }
 
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
