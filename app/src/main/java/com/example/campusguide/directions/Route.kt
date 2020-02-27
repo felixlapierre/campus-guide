043d8f495163d938911d8215dee1fd7aa54f084b
Sharon Ho <sharon.cy.ho@gmail.com>
2020-02-27 17:47:21 -0500

    Added description snippets to markers and removed previous markers

--- app/src/main/java/com/example/campusguide/directions/Route.kt
+++ app/src/main/java/com/example/campusguide/directions/Route.kt
@@ -4,13 +4,10 @@ import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import com.example.campusguide.utils.MessageDialogFragment
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Polyline
-import com.google.android.gms.maps.model.PolylineOptions
+import com.google.android.gms.maps.model.*
 import com.google.maps.internal.PolylineEncoding
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import com.google.android.gms.maps.model.MarkerOptions
 
 
 /**
@@ -22,23 +19,40 @@ import com.google.android.gms.maps.model.MarkerOptions
  */
 class Route constructor(private val map: GoogleMap, private val activity: AppCompatActivity) {
     private var polyline: Polyline? = null
+    private var begin: Marker? = null
+    private var dest: Marker? = null
+
+    private fun capitalizeWords (location: String): String{
+        val words = location.split(" ").toMutableList()
+        var output = ""
+        for (word in words){
+            output += word.capitalize() + " "
+        }
+        output = output.trim()
+        return output
+    }
 
     fun set(start: String, end: String) {
         polyline?.remove()
+        begin?.remove()
+        dest?.remove()
         val directions = Directions(activity)
 
         //Create a coroutine so we can invoke the suspend function Directions::getDirections
         GlobalScope.launch {
-            val apiKey = activity.resources.getString(com.example.campusguide.R.string.google_maps_key)
             val response = directions.getDirections(start, end)
+            val startCapitalized = capitalizeWords(start)
+            val endCapitalized = capitalizeWords(end)
             if(response != null) {
                 response.routes[0]
                 val startLat = response.routes[0].legs[0].startLocation.lat
                 val startLng = response.routes[0].legs[0].startLocation.lng
                 val endLat = response.routes[0].legs[0].endLocation.lat
                 val endLng = response.routes[0].legs[0].endLocation.lng
-                val start = MarkerOptions().position(LatLng(startLat.toDouble(), startLng.toDouble())).title("Start")
-                val dest = MarkerOptions().position(LatLng(endLat.toDouble(), endLng.toDouble())).title("Destination");
+                val startPoint = MarkerOptions().position(LatLng(startLat.toDouble(), startLng.toDouble()))
+                    .title(startCapitalized).snippet("Start")
+                val endPoint = MarkerOptions().position(LatLng(endLat.toDouble(), endLng.toDouble()))
+                    .title(endCapitalized).snippet("Destination")
                 val line = response.routes[0].overviewPolyline.points
                 val decoded = PolylineEncoding.decode(line)
 
@@ -56,8 +70,8 @@ class Route constructor(private val map: GoogleMap, private val activity: AppCom
                  */
                 activity.runOnUiThread {
                     polyline = map.addPolyline(PolylineOptions().addAll(decodedAsGoodLatLng))
-                    map.addMarker(start)
-                    map.addMarker(dest)
+                    begin = map.addMarker(startPoint)
+                    dest = map.addMarker(endPoint)
                 }
             }
         }
