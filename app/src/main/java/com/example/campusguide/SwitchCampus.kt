c6524409941a5eae9ea2e83ef12f1c431649f90b
Mike Brichko <w.mike.brichko@gmail.com>
2020-03-01 18:42:29 -0500

    Added location search feature (#51)
    * Added autocomplete google search functionnality
    
    * Added centering search function
    
    * Added focus from search button to EditText search locaion component
    
    * Testing gitignore changes
    
    * Removed dependencies and unnecessary files
    
    * Removed libraries and codeStyles
    
    * Removed files
    
    * Added Google Autocomplete search with geocode location
    
    * Removed .idea folder from app folder
    
    * Replaced autocomplete fragment with autocomplete intent
    
    * Deleted unnecessary files and turned values into constants
    
    * Fixed merge conflict and updated NDK
    
    Co-authored-by: Felix Lapierre <32281260+felixlapierre@users.noreply.github.com>

--- app/src/main/java/com/example/campusguide/SwitchCampus.kt
+++ app/src/main/java/com/example/campusguide/SwitchCampus.kt
@@ -1,5 +1,6 @@
 package com.example.campusguide
 
+import android.widget.TextView
 import android.widget.ToggleButton
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -7,24 +8,28 @@ import com.google.android.gms.maps.model.LatLng
 
 class SwitchCampus {
 
-    private lateinit var mMap : GoogleMap;
+    private lateinit var mMap : GoogleMap
 
-    constructor(toggleBtn: ToggleButton, mMap: GoogleMap) {
+    constructor(
+        toggleBtn: ToggleButton,
+        mMap: GoogleMap,
+        campusName: TextView
+    ) {
         this.mMap = mMap
         toggleBtn.setOnCheckedChangeListener{ _, isAtDowntownCampus ->
-            switchCampus(isAtDowntownCampus)
+            switchCampus(isAtDowntownCampus, campusName)
         }
     }
 
-    private fun switchCampus(isAtDowntownCampus: Boolean) {
+    private fun switchCampus(isAtDowntownCampus: Boolean, campusName: TextView) {
         if(isAtDowntownCampus){
             val loyCoord = LatLng(45.458153, -73.640490)
             mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(loyCoord, Constants.ZOOM_STREET_LVL))
-
+            campusName.text = Constants.LOYOLA_CAMPUS
         } else {
             val sgwCoord = LatLng(45.495792, -73.578096)
             mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sgwCoord, Constants.ZOOM_STREET_LVL))
+            campusName.text = Constants.SGW_CAMPUS
         }
     }
-
 }
