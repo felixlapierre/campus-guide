058760ee3b8d77bc0e8f7a33526289de0f1d1f83
Sharon Ho <33813045+sharon-ho@users.noreply.github.com>
2020-03-01 18:49:16 -0500

    Added color, pattern, zoom and start/end markers to the navigation path (#68)
    * Added start and destination markers to the navigation path
    
    * Readded earlier commit of markers for start and destination
    
    * Removed unnecessary imports in the Route file
    
    * Added description snippets to markers and removed previous markers
    
    * Polyline now returns a dashed blue line
    
    * Camera is centered to the center of the route's LatLng bounds
    
    * Refactored to have less variables
    
    * Changed "Start" and "Destination" to string constants

--- app/src/main/java/com/example/campusguide/directions/Route.kt
+++ app/src/main/java/com/example/campusguide/directions/Route.kt
@@ -1,15 +1,18 @@
 package com.example.campusguide.directions
 
-import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
-import com.example.campusguide.utils.MessageDialogFragment
+import com.example.campusguide.Constants
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Polyline
-import com.google.android.gms.maps.model.PolylineOptions
+import com.google.android.gms.maps.model.*
 import com.google.maps.internal.PolylineEncoding
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import com.google.android.gms.maps.model.Dash
+import com.google.android.gms.maps.model.PatternItem
+import com.google.android.gms.maps.model.Gap
+import com.google.android.gms.maps.model.PolylineOptions
+import com.google.android.gms.maps.model.LatLngBounds
 
 /**
  * Represents a route between two coordinates on the map.
@@ -19,21 +22,91 @@ import kotlinx.coroutines.launch
  * @param end The name of the location where the route ends
  */
 class Route constructor(private val map: GoogleMap, private val activity: AppCompatActivity) {
+    private val PATTERN_DASH_LENGTH_PX = 20
+    private val PATTERN_GAP_LENGTH_PX = 20
+    private val DASH: PatternItem = Dash(PATTERN_DASH_LENGTH_PX.toFloat())
+    private val GAP: PatternItem = Gap(PATTERN_GAP_LENGTH_PX.toFloat())
+    private val PATTERN_POLYGON_ALPHA = listOf(GAP, DASH)
+    private val COLOR_BLUE_ARGB = 0xff0000ff
     private var polyline: Polyline? = null
+    private var begin: Marker? = null
+    private var dest: Marker? = null
+    private val startString: String = "Start"
+    private val destString: String = "Destination"
+
+    private fun capitalizeWords(location: String): String
+    {
+        val words = location.split(" ").toMutableList()
+        var output = ""
+        for (word in words) {
+            output += word.capitalize() + " "
+        }
+        output = output.trim()
+        return output
+    }
 
     fun set(start: String, end: String) {
         polyline?.remove()
-
+        begin?.remove()
+        dest?.remove()
         val directions = Directions(activity)
 
         //Create a coroutine so we can invoke the suspend function Directions::getDirections
         GlobalScope.launch {
             val response = directions.getDirections(start, end)
-
-            if(response != null) {
+            if (response != null) {
+                val startPoint = MarkerOptions().position(
+                    LatLng(
+                        response.routes[0].legs[0].startLocation.lat.toDouble(),
+                        response.routes[0].legs[0].startLocation.lng.toDouble()
+                    )
+                ).title(capitalizeWords(start)).snippet(startString)
+                val endPoint = MarkerOptions().position(
+                    LatLng(
+                        response.routes[0].legs[0].endLocation.lat.toDouble(),
+                        response.routes[0].legs[0].endLocation.lng.toDouble()
+                    )
+                ).title(capitalizeWords(end)).snippet(destString)
+                val routeBounds = LatLngBounds(
+                    LatLng(
+                        response.routes[0].bounds.southwest.lat.toDouble(),
+                        response.routes[0].bounds.southwest.lng.toDouble()
+                    ),
+                    LatLng(
+                        response.routes[0].bounds.northeast.lat.toDouble(),
+                        response.routes[0].bounds.northeast.lng.toDouble()
+                    )
+                )
                 val line = response.routes[0].overviewPolyline.points
+                val decoded = PolylineEncoding.decode(line)
 
-                runAddPolyline(decodeLine(line))
+                /**
+                 * The com.google package contains two different representations of the
+                 * LatLng class. The decoded points must be converted to the representation
+                 * in com.google.android for PolylineOptions to accept them.
+                 */
+                val decodedAsGoodLatLng = decoded.map {
+                    LatLng(it.lat, it.lng)
+                }
+
+                /**
+                 * addPolyline throws an exception if it is not run on the Ui thread.
+                 */
+                activity.runOnUiThread {
+                    val polyOptions = PolylineOptions()
+                        .color(COLOR_BLUE_ARGB.toInt())
+                        .pattern(PATTERN_POLYGON_ALPHA)
+                        .addAll(decodedAsGoodLatLng)
+                    polyline = map.addPolyline(polyOptions)
+                    begin = map.addMarker(startPoint)
+                    dest = map.addMarker(endPoint)
+                    map.moveCamera(
+                        CameraUpdateFactory.newLatLngZoom(
+                            routeBounds.center,
+                            Constants.ZOOM_STREET_LVL
+                        )
+                    )
+                }
             }
         }
     }
