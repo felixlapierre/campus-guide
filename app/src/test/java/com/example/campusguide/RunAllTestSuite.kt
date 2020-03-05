51c743d045a4fd4bb8f2daf5067b7f76c136d34e
Felix <felix.lapierre1@gmail.com>
2020-03-05 14:55:14 -0500

    Fixed some remaining merge conflicts

--- app/src/test/java/com/example/campusguide/RunAllTestSuite.kt
+++ app/src/test/java/com/example/campusguide/RunAllTestSuite.kt
@@ -1,6 +1,5 @@
 package com.example.campusguide
 
-<<<<<<< HEAD
 import com.example.campusguide.directions.CallbackDirectionsConfirmTest
 import com.example.campusguide.directions.DirectionsTest
 import com.example.campusguide.directions.EmptyDirectionsGuardTest
@@ -8,11 +7,6 @@ import com.example.campusguide.utils.DatabaseTest
 import com.example.campusguide.utils.MessageDialogFragmentTest
 import com.example.campusguide.utils.request.ApiKeyRequestDecoratorTest
 import com.example.campusguide.utils.request.RequestQueueTest
-=======
-import com.example.campusguide.directions.EmptyDirectionsGuardTest
-import com.example.campusguide.utils.DatabaseTest
-import com.example.campusguide.utils.RequestQueueTest
->>>>>>> 973f2e3c5e1955b6153f6462568db5ecb5780ea7
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
@@ -20,15 +14,11 @@ import org.junit.runners.Suite
 @Suite.SuiteClasses(
     EmptyDirectionsGuardTest::class,
     DatabaseTest::class,
-<<<<<<< HEAD
     RequestQueueTest::class,
     DirectionsTest::class,
     ApiKeyRequestDecoratorTest::class,
     MessageDialogFragmentTest::class,
     CallbackDirectionsConfirmTest::class
-=======
-    RequestQueueTest::class
->>>>>>> 973f2e3c5e1955b6153f6462568db5ecb5780ea7
 )
 
 class RunAllTestSuite
