a3d0398f6cc6f24d5d74c8ce89220c490d62e086
arejayelle <35614138+arejayelle@users.noreply.github.com>
2020-03-06 19:25:16 -0500

    Added display floorplan to Maps Initializer

--- app/src/main/java/com/example/campusguide/map/GoogleMapInitializer.kt
+++ app/src/main/java/com/example/campusguide/map/GoogleMapInitializer.kt
@@ -1,5 +1,7 @@
 package com.example.campusguide.map
 
+import android.graphics.Bitmap
+import android.graphics.Picture
 import androidx.fragment.app.FragmentActivity
 import com.example.campusguide.Constants
 import com.example.campusguide.R
@@ -8,7 +10,8 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.*
+import kotlinx.android.synthetic.main.activity_maps.*
 
 class GoogleMapInitializer constructor(private val activity: FragmentActivity, private val wrapper: GoogleMapAdapter): OnMapReadyCallback {
     init {
@@ -22,12 +25,27 @@ class GoogleMapInitializer constructor(private val activity: FragmentActivity, p
         if(map != null) {
             wrapper.adapted = map
             map.uiSettings.isMyLocationButtonEnabled = false
+            map.uiSettings.isIndoorLevelPickerEnabled = false
+            map.isIndoorEnabled = false;
+            map.isBuildingsEnabled = false
+
 
             // Center the map on Hall building
             val hall = LatLng(45.497290, -73.578824)
             map.animateCamera(CameraUpdateFactory.newLatLngZoom(hall,
                 Constants.ZOOM_STREET_LVL
             ))
+
+
+            val h = LatLng(45.4972695, -73.57894175)
+
+            map.addGroundOverlay(
+                GroundOverlayOptions()
+                    .image(BitmapDescriptorFactory.fromAsset("Hall7.bmp"))
+                    .position(h, 68F, 63F).bearing(124F)
+                    .zIndex(5F)
+                    .visible(true)
+                    )
 
             BuildingHighlights(map).addBuildingHighlights()
 
