d24edcf4bd1d83c84601290e6a2600e00e8ba8fc
gdzooks <issa4044@gmail.com>
2020-03-08 21:36:36 -0400

    Implement actual minimally working drawer menu.

--- app/src/main/java/com/example/campusguide/MapsActivity.kt
+++ app/src/main/java/com/example/campusguide/MapsActivity.kt
@@ -10,17 +10,43 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.campusguide.utils.permissions.Permissions
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.activity_maps.*
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
-class MapsActivity : AppCompatActivity() {
+
+
+class MapsActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private val permissions = Permissions(this)
     private lateinit var onSearchListener: View.OnClickListener
     private val activityResultListeners: MutableList<ActivityResultListener> = mutableListOf()
+
+    private lateinit var toolbar: Toolbar
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
 
         Bootstrapper(this)
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navView = findViewById(R.id.nav_view)
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, 0, 0
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        navView.setNavigationItemSelectedListener(this)
     }
 
     fun getCampusNameTextView(): TextView {
@@ -69,5 +95,27 @@ class MapsActivity : AppCompatActivity() {
     fun setOnNavigateListener(listener: View.OnClickListener) {
         val navigateButton: FloatingActionButton = findViewById(R.id.navigateButton)
         navigateButton.setOnClickListener(listener)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+//            R.id.nav_profile -> {
+//                Toast.makeText(this, "Profile clicked", Toast.LENGTH_SHORT).show()
+//            }
+//            R.id.nav_messages -> {
+//                Toast.makeText(this, "Messages clicked", Toast.LENGTH_SHORT).show()
+//            }
+//            R.id.nav_friends -> {
+//                Toast.makeText(this, "Friends clicked", Toast.LENGTH_SHORT).show()
+//            }
+//            R.id.nav_update -> {
+//                Toast.makeText(this, "Update clicked", Toast.LENGTH_SHORT).show()
+//            }
+//            R.id.nav_logout -> {
+//                Toast.makeText(this, "Sign out clicked", Toast.LENGTH_SHORT).show()
+//            }
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
