492699bdf66179b984572269aa05c05d8fc4f465
Maxim Pobudzey <maxim.pobudzey@net4user.com>
2020-03-15 22:08:35 -0400

    Added DirectionsActivity activity

---
+++ app/src/main/java/com/example/campusguide/DirectionsActivity.kt
@@ -0,0 +1,52 @@
+package com.example.campusguide
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
+import androidx.appcompat.app.AppCompatActivity
+
+import com.example.campusguide.directions.Route
+import com.example.campusguide.map.GoogleMapAdapter
+import com.example.campusguide.map.GoogleMapInitializer
+
+class DirectionsActivity : AppCompatActivity() {
+
+    private lateinit var map: GoogleMapAdapter
+    private lateinit var route: Route
+    private lateinit var start: String
+    private lateinit var end: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_directions)
+
+        map = GoogleMapAdapter()
+        GoogleMapInitializer(this, map, "directions_activity_map")
+
+        // Extract origin and destination from the intent
+        start = intent.getStringExtra("Origin")!!
+        end = intent.getStringExtra("Destination")!!
+
+        // Set the text field of the TextViews
+        findViewById<TextView>(R.id.origin).apply {
+            text = start
+        }
+        findViewById<TextView>(R.id.destination).apply {
+            text = end
+        }
+
+        // Display directions as soon as the activity gets created
+        route = Route(map, this)
+        route.set(start, end)
+
+    }
+
+    /**
+     * Called when the Back button is clicked.
+     */
+    fun finishActivity(view: View) {
+        finish()
+    }
+}
