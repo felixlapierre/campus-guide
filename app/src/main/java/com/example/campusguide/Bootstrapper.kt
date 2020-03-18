c9bfbf028ebd009a23e2e1a0523a805f1a616069
Felix <felix.lapierre1@gmail.com>
2020-03-18 15:00:17 -0400

    Addressed code review comments

--- app/src/main/java/com/example/campusguide/Bootstrapper.kt
+++ app/src/main/java/com/example/campusguide/Bootstrapper.kt
@@ -8,7 +8,6 @@ import com.example.campusguide.location.SwitchCampus
 import com.example.campusguide.map.GoogleMapAdapter
 import com.example.campusguide.map.GoogleMapInitializer
 import com.example.campusguide.search.CustomSearch
-import com.example.campusguide.search.Search
 import com.example.campusguide.utils.permissions.Permissions
 import database.ObjectBox
 
