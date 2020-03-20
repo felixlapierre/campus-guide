8b7674ecf3b424edd9e5efa7d3faf2c44f7642bc
gdzooks <issa4044@gmail.com>
2020-03-20 18:59:50 -0400

    Revert "Skeleton response handling for calendarlist requests"
    This reverts commit 4088b89e10847989b04287568eb955669fb2f1a0.

--- app/src/main/java/com/example/campusguide/calendar/GoogleCalendarAPIResponseParser.kt
+++
@@ -1,7 +0,0 @@
-package com.example.campusguide.calendar
-
-import org.json.JSONObject
-
-interface GoogleCalendarAPIResponseParser {
-    fun parse(response: JSONObject): GoogleCalendarAPIResponse?
-}
--- app/src/main/java/com/example/campusguide/calendar/GoogleCalendarListAPIResponseParser.kt
+++
@@ -1,7 +0,0 @@
-package com.example.campusguide.calendar
-
-import org.json.JSONObject
-
-interface GoogleCalendarListAPIResponseParser {
-    fun parse(response: JSONObject): GoogleCalendarListAPIResponse?
-}
