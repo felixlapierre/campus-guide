2ab9a58aa84bb38893157595702025f6459a4f07
w.mike.brichko@gmail.com <w.mike.brichko@gmail.com>
2020-03-24 09:24:25 -0400

    Added custom popup window

--- app/src/main/java/com/example/campusguide/search/CurrentTravelPopup.kt
+++ app/src/main/java/com/example/campusguide/search/CurrentTravelPopup.kt
@@ -1,10 +1,9 @@
 package com.example.campusguide.search
 
-import android.content.Intent
 import android.view.Gravity
 import android.view.View
+import android.view.WindowManager
 import android.widget.PopupWindow
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.campusguide.R
 import com.google.android.libraries.places.api.model.Place
@@ -13,28 +12,27 @@ import kotlinx.android.synthetic.main.confirm_window.view.*
 class CurrentTravelPopup {
     private var view : View
     private var activity: AppCompatActivity
-    private var window: PopupWindow
+    private var popup: PopupWindow
 
     constructor(activity: AppCompatActivity, place: Place){
         this.activity = activity
-        window = PopupWindow(activity)
-        view = activity.layoutInflater.inflate(R.layout.confirm_window, null)
+        popup = PopupWindow(activity)
+        popup.setBackgroundDrawable(activity.resources.getDrawable(R.drawable.custom_triangular_shape))
+        popup.height = WindowManager.LayoutParams.WRAP_CONTENT
+        popup.width = WindowManager.LayoutParams.WRAP_CONTENT
+        popup.isOutsideTouchable = true;
+        popup.isFocusable = true;
 
-        window.contentView = view
-        window.showAtLocation(
+        view = activity.layoutInflater.inflate(R.layout.confirm_window, null)
+        popup.contentView = view
+
+        popup.showAtLocation(
             activity.findViewById(android.R.id.content),
             Gravity.CENTER_VERTICAL,
             20,
             20)
 
-        view.locationSearched.text = place.name!!
-
-        view.confirm_button.setOnClickListener{
-
-        }
-
-        view.cancel_button.setOnClickListener{
-            window.dismiss()
-        }
+        view.primaryText.text = place.name!!
+        view.secondaryText.text = place.address
     }
 }
