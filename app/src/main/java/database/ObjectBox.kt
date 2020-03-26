ebd114d8e4b9ddb588d2b587b662281e1429c02a
FGRCL <35940434+FGRCL@users.noreply.github.com>
2020-03-26 11:42:10 -0400

    Added a prebuilt database as an asset for the application

--- app/src/main/java/database/ObjectBox.kt
+++ app/src/main/java/database/ObjectBox.kt
@@ -5,11 +5,15 @@ import database.entity.MyObjectBox
 import io.objectbox.BoxStore
 
 object ObjectBox {
+    val DB_DIRECTORY_NAME = "database"
+    val DB_FILE_NAME  = "data.mdb"
+
     lateinit var boxStore: BoxStore
         private set
 
     fun init(context: Context) {
         boxStore = MyObjectBox.builder()
+            .initialDbFile(DatabaseStreamFactory(context.assets.open(DB_DIRECTORY_NAME+"/"+DB_FILE_NAME)))
             .androidContext(context.applicationContext)
             .build()
     }
