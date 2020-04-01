72cd0764491855fa49972228c889e8d7ffb34c0c
Felix <felix.lapierre1@gmail.com>
2020-04-01 00:05:30 -0400

    Removed duplicate map interface (which I had accidentally renamed to ap

--- app/src/main/java/com/example/campusguide/Map.kt
+++
@@ -1,8 +0,0 @@
-package com.example.campusguide
-
-import com.google.android.gms.maps.model.LatLng
-
-interface ap {
-    fun addMarker(position: LatLng, title: String)
-    fun animateCamera(position: LatLng, zoom: Float)
-}
