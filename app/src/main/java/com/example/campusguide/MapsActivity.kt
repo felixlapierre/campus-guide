ba1f6148c0ebc66ac0f479a461972776be2501ac
FGRCL <35940434+FGRCL@users.noreply.github.com>
2020-04-07 14:26:18 -0400

    Added blank lines to test the travis-ci

--- app/src/main/java/com/example/campusguide/MapsActivity.kt
+++ app/src/main/java/com/example/campusguide/MapsActivity.kt
@@ -22,6 +22,8 @@ class MapsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
 
+
+        
         Bootstrapper(this)
     }
 
