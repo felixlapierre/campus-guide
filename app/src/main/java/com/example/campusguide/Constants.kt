fbf11aa54aa0f835102aae2d2f71c32841f5bf08
Jun Loke <rebecca.j.loke@gmail.com>
2020-04-11 19:57:05 -0400

    added Constant for indoorZoom level

--- app/src/main/java/com/example/campusguide/Constants.kt
+++ app/src/main/java/com/example/campusguide/Constants.kt
@@ -2,6 +2,7 @@ package com.example.campusguide
 
 object Constants {
     const val ZOOM_STREET_LVL = 17.0f;
+    const val ZOOM_INDOOR_LVL = 18.0f;
     const val PERMISSION_REQUEST_CODE = 1
     const val AUTOCOMPLETE_REQUEST_CODE = 2
     const val LOYOLA_CAMPUS = "Loyola Campus"
