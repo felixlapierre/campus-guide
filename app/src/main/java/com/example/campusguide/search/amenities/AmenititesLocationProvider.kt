ac4c274b4082ec3540580086aafd4359d73bfc86
bogalla <brianne.ogallagher@gmail.com>
2020-04-10 22:41:48 -0400

    basic working prototype with pathfinding

--- app/src/main/java/com/example/campusguide/search/amenities/AmenititesLocationProvider.kt
+++ app/src/main/java/com/example/campusguide/search/amenities/AmenititesLocationProvider.kt
@@ -3,10 +3,12 @@ package com.example.campusguide.search.amenities
 import androidx.appcompat.app.AppCompatActivity
 import com.example.campusguide.Constants
 import com.example.campusguide.directions.ChooseOriginOptions
+import com.example.campusguide.directions.indoor.Graph
 import com.example.campusguide.location.FusedLocationProvider
 import com.example.campusguide.location.Location
 import com.example.campusguide.search.SearchLocation
 import com.example.campusguide.search.SearchLocationProvider
+import com.example.campusguide.search.indoor.BuildingIndexSingleton
 import com.example.campusguide.utils.permissions.Permissions
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
@@ -19,14 +21,24 @@ class AmenitiesLocationProvider constructor(
 ) : SearchLocationProvider {
     override suspend fun getLocation(id: String): SearchLocation? {
         val isAmenities = id.startsWith(Constants.AMENITIES_LOCATION_IDENTIFIER)
+        val lbBuildingIndex =  BuildingIndexSingleton.getInstance(activity.assets).findBuildingByCode("LB")!!
         println("-------------------------id: " + id)
         println("-------------------------is amenities: " + isAmenities)
 
         if(isAmenities) {
             println("-------------------------WOW amenities!")
-            val origin = getOrigin()
-
-            return getAmenities()
+            val origin = getOrigin().encodeForDirections()
+            var start = ""
+            if (origin.startsWith(Constants.INDOOR_LOCATION_IDENTIFIER)) {
+                start = origin.split("_")[2]
+            } else {
+                start = lbBuildingIndex.nodes[0].code
+            }
+            val listOfBathrooms = AmenitiesPathfinding(
+                Graph(
+                   lbBuildingIndex
+                )).findRoom(start)
+            return SearchLocation("Bathroom", listOfBathrooms[0][listOfBathrooms[0].size - 1].latitude, listOfBathrooms[0][listOfBathrooms[0].size - 1].longitude, "amenities_bathroom", "")
         } else {
             return next?.getLocation(id)
         }
@@ -37,9 +49,5 @@ class AmenitiesLocationProvider constructor(
             cont.resume(origin)
         }
         chooseOriginOptions.show(activity.supportFragmentManager, "chooseOriginOptions")
-    }
-
-    private fun getAmenities(): SearchLocation {
-        return SearchLocation("name", 41.11, 75.22, "amenities_bathroom", "secondary text")
     }
 }
