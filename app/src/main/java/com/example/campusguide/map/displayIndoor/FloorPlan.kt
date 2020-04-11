00e6340fd479a17929a4a672bfbbe66eabfcf20e
arejayelle <35614138+arejayelle@users.noreply.github.com>
2020-04-11 11:50:35 -0400

    changed BMP to svg drawables

---
+++ app/src/main/java/com/example/campusguide/map/displayIndoor/FloorPlan.kt
@@ -0,0 +1,13 @@
+package com.example.campusguide.map.displayIndoor
+
+import com.beust.klaxon.Json
+import com.google.android.gms.maps.model.GroundOverlay
+import com.google.android.gms.maps.model.LatLng
+
+class FloorPlan() {
+//    private val buildingCoordinates = @Json(name = )
+//    private var currentFloor = 0
+//    private var floors = HashMap<Int, GroundOverlay>()
+//
+
+}
