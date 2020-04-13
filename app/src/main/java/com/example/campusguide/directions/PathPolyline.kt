04202147ea6c92c1f716b8eee25234b402e5510e
Sharon Ho <33813045+sharon-ho@users.noreply.github.com>
2020-04-12 22:29:45 -0400

    Changed colors of navigation path and start/destination markers when getting directions (#183)
    * Changed color of polyline
    
    * Changed start/destination markers and path to different colors
    
    * Refactored pattern dash/gap lengths and primary/accent colors as constants
    
    * Fixed merge issues with master
    
    * Changed polyline color to azure
    
    * Removed unused imports in PathPolyline

--- app/src/main/java/com/example/campusguide/directions/PathPolyline.kt
+++ app/src/main/java/com/example/campusguide/directions/PathPolyline.kt
@@ -1,8 +1,11 @@
 package com.example.campusguide.directions
 
+import android.graphics.Color
+import com.example.campusguide.Constants
 import com.example.campusguide.map.Map
 import com.example.campusguide.map.Marker
 import com.example.campusguide.utils.Helper
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.Dash
 import com.google.android.gms.maps.model.Gap
 import com.google.android.gms.maps.model.LatLng
@@ -17,12 +20,10 @@ import kotlinx.coroutines.async
 
 class PathPolyline constructor(startName: String, endName: String, val segment: Segment) {
     class PolylineStyle {
-        private val patternDashLengthPx = 20.0f
-        private val patternGapLengthPx = 20.0f
-        private val patternDash: PatternItem = Dash(patternDashLengthPx)
-        private val patternGap: PatternItem = Gap(patternGapLengthPx)
+        private val patternDash: PatternItem = Dash(Constants.PATTERN_DASH_LENGTH_PX)
+        private val patternGap: PatternItem = Gap(Constants.PATTERN_GAP_LENGTH_PX)
         val patternPolygonAlpha = listOf(patternGap, patternDash)
-        val colorBlueArgb = 0xff0000ff
+        val pathColor = Color.parseColor(Constants.AZURE_COLOR)
     }
 
     private lateinit var path: List<LatLng>
@@ -45,13 +46,14 @@ class PathPolyline constructor(startName: String, endName: String, val segment: 
         deferred = GlobalScope.async {
             path = segment.toListOfCoordinates()
             polylineOptions.addAll(path)
-                .color(style.colorBlueArgb.toInt())
+                .color(style.pathColor)
                 .pattern(style.patternPolygonAlpha)
 
             val firstPoint = path[0]
             startMarkerOptions.position(firstPoint)
                 .title(Helper.capitalizeWords(startName))
                 .snippet("Start")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
 
             val lastPoint = path[path.size - 1]
             endMarkerOptions.position(lastPoint)
