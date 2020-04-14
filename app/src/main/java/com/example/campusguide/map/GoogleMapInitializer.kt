6f73b6afb3be34470069367df55abee963709765
Jun Loke <rebecca.j.loke@gmail.com>
2020-04-14 17:05:09 -0400

    Removed useless import

--- app/src/main/java/com/example/campusguide/map/GoogleMapInitializer.kt
+++ app/src/main/java/com/example/campusguide/map/GoogleMapInitializer.kt
@@ -10,7 +10,6 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
-import kotlinx.android.synthetic.main.activity_maps.*
 
 class GoogleMapInitializer constructor(private val activity: FragmentActivity,
                                        private val wrapper: GoogleMapAdapter,
