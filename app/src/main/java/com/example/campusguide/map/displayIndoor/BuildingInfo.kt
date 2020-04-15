3ee57d525975bccd6958ebefd12e7b5d2692d5fd
Jun Loke <rebecca.j.loke@gmail.com>
2020-04-14 23:21:29 -0400

    Moved duplicate code into function as per code review

--- app/src/main/java/com/example/campusguide/map/displayIndoor/BuildingInfo.kt
+++ app/src/main/java/com/example/campusguide/map/displayIndoor/BuildingInfo.kt
@@ -6,67 +6,60 @@ import com.google.android.gms.maps.model.GroundOverlay
 import com.google.android.gms.maps.model.GroundOverlayOptions
 import com.google.android.gms.maps.model.LatLng
 
-class BuildingInfo constructor(buildingName: String, map: GoogleMapAdapter) {
+class BuildingInfo constructor(private val buildingName: String, map: GoogleMapAdapter) {
+    private val floors: IntArray? = setFloors()
+    private val buildingImageCoordinates: LatLng = setBuildingImageCoordinates()
+    private val floorPlans: HashMap<Int, GroundOverlay>? = setUpFloorPlans(map)
+    val startFloor: Int? = floors?.get(0)
 
-    private val floors: IntArray? = setFloors(buildingName)
-        fun getFloors(): IntArray? {
-            return floors
-        }
-    private val buildingImageCoordinates: LatLng = setBuildingImageCoordinates(buildingName)
+    fun getFloors(): IntArray? {
+        return floors
+    }
     fun getBuildingImageCoordinates(): LatLng {
         return buildingImageCoordinates
     }
-    private val floorPlans: HashMap<Int, GroundOverlay>? = setUpFloorPlans(buildingName, map)
-        fun getFloorPlans(): HashMap<Int, GroundOverlay>? {
-            return floorPlans
-        }
+    fun getFloorPlans(): HashMap<Int, GroundOverlay>? {
+        return floorPlans
+    }
 
-    val startFloor: Int? = floors?.get(0)
-
-    private fun setBuildingImageCoordinates(buildingName: String): LatLng {
+    private fun setBuildingImageCoordinates(): LatLng {
         if (buildingName == "hall") return LatLng(45.4972695, -73.57894175)
         if (buildingName == "library") return LatLng(45.496753, -73.577904)
 
         return LatLng(0.0, 0.0)
     }
-    private fun setFloors(buildingName: String): IntArray? {
+    private fun setFloors(): IntArray? {
         if (buildingName == "hall") return intArrayOf(4, 5, 6, 7, 8)
         if (buildingName == "library") return intArrayOf(2, 3, 4, 5)
 
         return null
     }
+    private fun setUpFloorPlans(map: GoogleMapAdapter): HashMap<Int, GroundOverlay>? {
 
-    private fun setUpFloorPlans(buildingName: String, map: GoogleMapAdapter): HashMap<Int, GroundOverlay>? {
-
-        if (floors == null) {
-            return null
+        if (floors != null) {
+            if (buildingName == "hall") {
+                return createGroundOverlays("h", map, 68F, 63F, 124F)
+            } else if (buildingName == "library") {
+                return createGroundOverlays("lb", map, 82F, 82F, -56F)
+            }
         }
+        return null
+    }
 
+    private fun createGroundOverlays(buildingCode: String, map: GoogleMapAdapter, length: Float, width: Float, bearing: Float): HashMap<Int, GroundOverlay> {
         val buildingFloors = hashMapOf<Int, GroundOverlay>()
 
-        if (buildingName == "hall") {
-
+        if (floors != null) {
             for (floor in floors) {
                 buildingFloors[floor] = map.adapted.addGroundOverlay(
                     GroundOverlayOptions()
-                        .image(BitmapDescriptorFactory.fromAsset("h_floor$floor.png"))
-                        .position(buildingImageCoordinates, 68F, 63F).bearing(124F)
-                        .visible(false)
-                        .zIndex(3F)
-                )
-            }
-        } else if (buildingName == "library") {
-            for (floor in floors) {
-                buildingFloors[floor] = map.adapted.addGroundOverlay(
-                    GroundOverlayOptions()
-                        .image(BitmapDescriptorFactory.fromAsset("lb_floor$floor.png"))
-                        .position(buildingImageCoordinates, 82F, 82F).bearing(-56F)
+                        .image(BitmapDescriptorFactory.fromAsset("${buildingCode}_floor$floor.png"))
+                        .position(buildingImageCoordinates, length, width).bearing(bearing)
                         .visible(false)
                         .zIndex(3F)
                 )
             }
         }
-
         return buildingFloors
     }
 }
